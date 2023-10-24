129dce7c01aaf2eebef8a618fd6aad71bdbc0282
MateuszMirek <matmirek24>
2023-10-24 11:08:55 +0200

    no ten C++

--- folder z C++/DEV C++.cpp
+++ folder z C++/DEV C++.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
+	short zmienna=10;
 	cout<<"Hello World!";
+	cout<<endl;
+	cout<<"Jakis super mega tekst";
+	cout<<zmienna;
 	return 0;
 	
 }
